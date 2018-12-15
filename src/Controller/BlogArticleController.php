0b97a51d65d66da1669759e0da7a766bec5d5098
Sébastien Vallet <sebastien.vallet89@gmail.com>
2018-12-15 11:55:09 +0100

    dev: added front for Blog

--- src/Controller/BlogArticleController.php
+++ src/Controller/BlogArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\Provider\BlogArticleProvider;
 
 /**
  * @Route("/blog", name="blog_")
@@ -13,20 +14,26 @@ class BlogArticleController extends AbstractController
     /**
      * @Route("/article", name="article_index")
      */
-    public function index()
+    public function index(BlogArticleProvider $provider)
     {
+
+        $articles = $provider->getAllBlogArticle();
+
         return $this->render('blog_article/index.html.twig', [
-            'controller_name' => 'BlogArticleController',
+            'articles' => $articles
         ]);
     }
 
     /**
      * @Route("/article/{id}", name="article_show")
      */
-    public function show()
+    public function show(BlogArticleProvider $provider, $id)
     {
-        return $this->render('blog_article/index.html.twig', [
-            'controller_name' => 'BlogArticleController',
+
+        $article = $provider->getOneById($id);
+
+        return $this->render('blog_article/show.html.twig', [
+            'article' => $article
         ]);
     }
 }
---
+++ src/Service/Handler/BlogArticleHandler.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Service\Handler;
+
+use App\Service\Handler\General\CommonHandler;
+use App\Entity\BlogArticle;
+
+class  BlogArticleHandler extends CommonHandler
+{
+	protected $blogArticleRepo;
+
+	public function initRepositories()
+	{
+		$this->blogArticleRepo = $this->em->getRepository('App:BlogArticle');
+	}
+
+	/**
+	 * Save a blogArticle
+	 *
+	 * @param BlogArticle $blogArticle
+	 * @param boolean $andFlush Automatically flush
+	 *
+	 * @return BlogArticle $blogArticle
+	 */
+	public function saveBlogArticle(BlogArticle $blogArticle, bool $andFlush = true)
+	{
+
+		// $blogArticle->setHeadlineSlug(uniqid());
+
+		$this->em->persist($blogArticle);
+		if ($andFlush) {
+			$this->em->flush();
+		}
+
+		return $blogArticle;
+	}
+
+	/**
+	 * Delete a blogArticle
+     *
+     * @param BlogArticle $blogArticle
+     *
+     * @return bool
+	 */
+	public function deleteBlogArticle(BlogArticle $blogArticle)
+	{
+		$this->em->remove($blogArticle);
+		$this->em->flush();
+
+		return true;
+	}
+
+	/**
+     * Delete the file
+     *
+     * @param BlogArticle $blogArticle
+     *
+     * @return bool
+     */
+    public function deleteFile(BlogArticle $blogArticle)
+    {
+        $blogArticle->setImage(null);
+
+        $this->em->flush();
+
+        return true;
+    }
+
+}
